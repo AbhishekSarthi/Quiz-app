1fe52bf203e356c149056ff88522afc50edd34be
Abhishek Sarthi <abhi.sarthi15@gmail.com>
2021-02-08 18:45:39 +0530

    second commit

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const requests = require('./routes/requests');
-
+const path = require('path');
 const mongoose = require('mongoose');
+require('dotenv/config');
 
 const app = express();
 
@@ -15,10 +16,21 @@ app.get('/', (req, res) => {
 //details page
 app.use('/details', requests);
 
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '/frontend/build')));
+
+    app.get('*', (req, res) => {
+        res.sendFile(
+            path.resolve(__dirname, 'frontend', 'build', 'index.html')
+        );
+    });
+} else {
+    app.listen(5000, console.log('Server running on port 5000'));
+}
+
 mongoose.connect(
-    'mongodb+srv://quiz:Edgxcxwd4dTj3GfD@cluster0.hqba7.mongodb.net/quiz?retryWrites=true&w=majority',
+    process.env.DB_CONNECTION,
     { useNewUrlParser: true, useUnifiedTopology: true },
     () => console.log('DB connected...')
 );
-
-app.listen(5000, console.log('Server running on port 5000'));
+// const __dirname = path.resolve();
